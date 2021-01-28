3f32e9dba9a69dcd9a9d1b1542246a5e4b246f48
RamneekKaur983 <ramneek983@gmail.com>
2021-01-28 21:59:13 +0530

    Added Download PDF option

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var ejs= require('ejs')
 var mongoose = require('mongoose')
 var nodemailer = require("nodemailer");
 const session = require('express-session')
-const flash=require('express-flash-messages') 
+const flash=require('express-flash-messages')
+const puppeteer = require("puppeteer"); 
 var spawn = require("child_process").spawn; 
 const {PythonShell} =require('python-shell');
 const path = require('path') 
@@ -61,6 +62,35 @@ app.get('/',function(req,res){
 app.get('/login' , function(req, res)
 {
     res.render('tabs.ejs')
+})
+app.get("/pdf", async (req, res) => {
+    const url = req.query.target;
+
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+
+    const webPage = await browser.newPage();
+
+    await webPage.goto(url, {
+        waitUntil: "networkidle0"
+    });
+    
+    const pdf = await webPage.pdf({
+        printBackground: true,
+        format: "Letter",
+        margin: {
+            top: "20px",
+            bottom: "40px",
+            left: "20px",
+            right: "20px"
+        }
+    });
+
+    await browser.close();
+
+    res.contentType("application/pdf");
+    res.send(pdf);
 })
 app.get('/display' , async function(req, res)
 {
